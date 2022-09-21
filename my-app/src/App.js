b549012fa1ad8b921fc40a56a2ecfebfd3437a65
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-09-21 23:33:21 +0530

    used useEffect

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import React ,{useState} from 'react'
+
+
+const App  = ()=>{
+  const [num, setNum] = useState(0)
+  const increase = ()=>{
+    setNum(num +1)
+  }
+  return(
+
+<>
+<h1>  {num} </h1>
+  <button onClick={increase} >click here</button>
+</>
+
+  )
+}
+
+export default App
